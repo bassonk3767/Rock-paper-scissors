4750e5fe5dafaa4ce39a1f86814235ed13106b36
bassonk3767 <145512278+bassonk3767@users.noreply.github.com>
2023-10-24 13:30:16 +1300

    Add files via upload

--- B_RPS_v01.py
+++ B_RPS_v01.py
@@ -21,16 +21,35 @@ def Choice_checker(question):
     print("\n\n        {}".format(statement_generator("Rock / "
           "Paper / Scissors", " | ", "*")))
 
+    error = "\n\n      Please choose from the options given. "
+
     while not False:
         response = input(question).lower()
         if response == "r" or response == "rock":
             return response
+        elif response == "p" or response == "paper":
+            return response
+        elif response == "s" or response == "scissors":
+            return response
 
+        # Check for exit code
+        elif response == "xxx":
+            return response
+        else:
+            print(error)
 
 # Main routine...
 
-# Ask the user for choice and check if it's valid
-user_choice = Choice_checker("\n\n      Please choose:"
-                             " Rock / Paper / Scissors\n\n      ~~~  ")
+# List for checking response
+rps_list = ["rock","paper","scissors"]
 
-# print out choice for comparison purposes
+# Loop for testing purposes
+user_choice = ""
+while user_choice !=  "xxx":
+
+    # Ask the user for choice and check if it's valid
+    user_choice = Choice_checker("\n\n      Please choose: Rock / Paper "
+                                 "/ Scissors. (or xxx)\n\n      ~~~  ")
+
+    # print out choice for comparison purposes
+    print("\n\n     You chose {}".format(user_choice))
