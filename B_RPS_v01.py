31817448678cc8e1be17c34912b5f13ec7fb2c69
bassonk3767 <145512278+bassonk3767@users.noreply.github.com>
2023-10-25 12:29:37 +1300

    Add files via upload

--- B_RPS_v01.py
+++ B_RPS_v01.py
@@ -17,40 +17,72 @@ def statement_generator(statement, side, top_bottom):
 
     return ""
 
-def Choice_checker(question):
-    print("\n\n        {}".format(statement_generator("Rock / "
-          "Paper / Scissors", " | ", "*")))
 
-    error = "\n\n      Please choose from the options given. "
+# Function that check the amount of rounds that the player wam=nts to play
+def check_rounds():
+    while True:
+        response = input("\n\n      How many rounds wou"
+                         "ld you like to play?\n\n     ~~~  ")
+        round_error = "\n\n     Please type either <enter> " \
+                      "or an integer  that is higher than 0"
+        if response != "":
+            try:
+                # converting input so that it can be checked by length
+                response = int(response)
 
-    while not False:
-        response = input(question).lower()
-        if response == "r" or response == "rock":
-            return response
-        elif response == "p" or response == "paper":
-            return response
-        elif response == "s" or response == "scissors":
-            return response
+                if response < 1:
+                    print(round_error)
+                    return response
 
-        # Check for exit code
-        elif response == "xxx":
-            return response
-        else:
-            print(error)
+            # If response by any chance has an unexpected error
+            except ValueError:
+                print(round_error)
+                continue
+
+        return response
+
 
 # Main routine...
+chosen = ""
 
-# List for checking response
-yes_no_list = []
-rps_list = ["rock","paper","scissors"]
+rounds_played = 0
+choose_instruction = "\n\n     Please choose rock" \
+                      " (r), paper (p), scissors (s)\n\n    ~~~ "
 
-# Loop for testing purposes
-user_choice = ""
-while user_choice !=  "xxx":
+# Ask user for # of rounds, and they can <enter> for infinite rounds mode
+rounds = check_rounds()
 
-    # Ask the user for choice and check if it's valid
-    user_choice = Choice_checker("\n\n      Please choose: Rock / Paper "
-                                 "/ Scissors. (or xxx)\n\n      ~~~  ")
+end_game = "no"
+while end_game == "no":
 
-    # print out choice for comparison purposes
-    print("\n\n     You chose {}".format(user_choice))
+    # Rounds Heading
+    print("\n\n     ")
+    if rounds == "":
+        heading = "Continuous Mode: Round #{}".format(rounds_played)
+        print(heading)
+        choose = input("\n\n    {} or 'xxx' to end".format(choose_instruction))
+        if choose == "xxx":
+            break
+    else:
+        heading = "\n\n     Round #{} of {}".format(rounds_played +1,rounds)
+        print(heading)
+        choose = input(choose_instruction)
+        if rounds_played == rounds - 1:
+            end_game = "yes"
+
+
+    # Rest of loop / game
+    if choose == "rock" or choose == "r":
+        chosen = "rock"
+
+    elif choose == "paper" or choose == "p":
+        chosen = "paper"
+
+    elif choose == "scissors" or choose == "s":
+        chosen = "scissors"
+
+    print("\n\n     You choose {}".format(chosen))
+
+    rounds_played += 1
+
+print("\n\n     Thank you for playing.")
