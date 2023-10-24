b56b1e2575dc40184d48a53183ae8ada7a414276
bassonk3767 <145512278+bassonk3767@users.noreply.github.com>
2023-10-25 12:22:27 +1300

    Add files via upload

--- S_round_mechanics.py
+++ S_round_mechanics.py
@@ -14,20 +14,57 @@ def check_rounds():
 
                 if response < 1:
                     print(round_error)
-                    continue
+                    return response
 
             # If response by any chance has an unexpected error
             except ValueError:
                 print(round_error)
                 continue
-    return ""
+
+        return response
 
 
 # Main routine...
+chosen = ""
 
 rounds_played = 0
-choose_instructions = "\n\n     Please choose rock" \
-                      " (r), paper (p), scissors (s)"
+choose_instruction = "\n\n     Please choose rock" \
+                      " (r), paper (p), scissors (s)\n\n    ~~~ "
 
 # Ask user for # of rounds, and they can <enter> for infinite rounds mode
 rounds = check_rounds()
+
+end_game = "no"
+while end_game == "no":
+
+    # Rounds Heading
+    print("\n\n     ")
+    if rounds == "":
+        heading = "Continuous Mode: Round #{}".format(rounds_played)
+        print(heading)
+        choose = input("\n\n    {} or 'xxx' to end".format(choose_instruction))
+        if choose == "xxx":
+            break
+    else:
+        heading = "\n\n     Round #{} of {}".format(rounds_played +1,rounds)
+        print(heading)
+        choose = input(choose_instruction)
+        if rounds_played == rounds - 1:
+            end_game = "yes"
+
+
+    # Rest of loop / game
+    if choose == "rock" or choose == "r":
+        chosen = "rock"
+
+    elif choose == "paper" or choose == "p":
+        chosen = "paper"
+
+    elif choose == "scissors" or choose == "s":
+        chosen = "scissors"
+
+    print("\n\n     You choose {}".format(chosen))
+
+    rounds_played += 1
+
+print("\n\n     Thank you for playing.")
